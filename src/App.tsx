828868ac8f480fd74e6be9e3caf8c5e340a28c55
Belle Hannah Sebial <114854900+bellesebial@users.noreply.github.com>
2022-11-22 00:06:50 +0800

    hshs

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
 import React from 'react';
 import './App.css';
+import { Button } from '@mui/material';
 import ReviewPage from './Review/ReviewPage';
-import Welcome from './UserComponent/welcome';
 
-const pages = ['Home', 'Reviews', 'Teknoy Books'];
 
-function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null); 
-
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
+function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <ReviewPage/>
       </header>
-      </div>
+    </div>
   );
+
 }
+
 
 export default App;
