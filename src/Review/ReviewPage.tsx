4016c5e1bc9ba6fbbfa870a227a171f7c58bfb7d
Belle Hannah Sebial <114854900+bellesebial@users.noreply.github.com>
2022-11-23 00:16:53 +0800

    kapoy

--- src/Review/ReviewPage.tsx
+++ src/Review/ReviewPage.tsx
@@ -5,53 +5,61 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { Typography } from '@mui/material';
+import { Breadcrumbs, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from '@mui/material';
+import Link from '@mui/material/Link';
 
-function createData(
+export interface createReview{
   cover: string,
   title: string,
   author: string,
   rating: number,
   review: string,
-) {
-  return { cover, title, author, rating, review };
 }
 
-const rows = [
-  createData('/images/uglylove.jpeg', 'Ugly Love', 'Colleen Hover', 3, 'Write a Review')
-];
+const rows: createReview[] = [
+  {cover:'images/uglylove.jpeg', title:'Ugly Love', author:'Colleen Hover', rating:3, review:'Nice book!'}
+]
 
-export default function BasicTable() {
+export default function ReviewPage(props:createReview) {
   return (
-    <TableContainer component={Paper} sx={{marginTop:3, marginLeft:100}}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Book Cover</TableCell>
-            <TableCell align="center">Title</TableCell>
-            <TableCell align="center">Author</TableCell>
-            <TableCell align="center">Rating</TableCell>
-            <TableCell align="center">Review</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.cover}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.cover}
-              </TableCell>
-              <TableCell align="center">{row.title}</TableCell>
-              <TableCell align="center">{row.author}</TableCell>
-              <TableCell align="center">{row.rating}</TableCell>
-              <TableCell align="center">{row.review}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+
+    <><Breadcrumbs separator="›" aria-label="breadcrumbs" sx={{ fontSize: 25, fontWeight:10, marginLeft: 5, marginTop: 3 }}>
+      {['User Review'].map((item: string) => (
+        <Link
+          key={item}
+          underline="hover"
+          color="orange"
+          fontSize="inherit"
+          fontWeight={10}
+          href="/"
+        >
+          {item}
+        </Link>
+      ))}
+    </Breadcrumbs>
+
+    <Typography
+    variant="h4"
+    component="a"
+    sx={{
+      mr: 2,
+      display: { xs: 'flex', md: 'flex' },
+      fontFamily: 'inherit',
+      fontWeight: 40,
+      fontSize: 25,
+      color: '',
+      marginLeft:8.5,
+      marginTop:4
+    }}
+    >Book Cover
+    </Typography>
+
+    <Card sx={{ width: 200, marginLeft: 4, marginBottom: '1rem', marginTop: '1rem', backgroundColor: 'white', color: 'black' }}>
+        <CardMedia
+          component="img"
+          height="250"
+          image={props.cover}
+          alt="book cover" />
+      </Card></>
   );
 }
---
+++ src/Review/WriteAReview.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import { Box, Breadcrumbs, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from '@mui/material';
+import Link from '@mui/material/Link';
+
+export interface createReview{
+  cover: string,
+  title: string,
+  author: string,
+  rating: number,
+  review: string,
+}
+
+const rows: createReview[] = [
+  {cover:'images/uglylove.jpeg', title:'Ugly Love', author:'Colleen Hover', rating:3, review:'Nice book!'}
+]
+
+function handleClick(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    event.preventDefault();
+    console.info('You clicked a breadcrumb.');
+  }
+
+export default function ReviewPage(props:createReview) {
+  return (
+    <><><div role="presentation" onClick={handleClick}>
+          <Breadcrumbs separator=">" aria-label="breadcrumb" sx={{ marginTop: 2, marginLeft: 4 }}>
+              <Link color="black" fontSize={25} fontWeight={10} underline="hover" href="/Review/ReviewPage">
+                  User Review
+              </Link>
+              <Link color="orange" fontSize={25} fontWeight={10} underline="hover" href="/">
+                  Write a Review
+              </Link>
+          </Breadcrumbs>
+      </div>
+
+          <Card sx={{ textAlign: 'center', width: 200, marginLeft: 4, marginBottom: '1rem', marginTop: '1rem', backgroundColor: 'white', color: 'black' }}>
+              <Box>
+                  <CardMedia component="img" height="250" sx={{ width: 200 }}
+                      image={props.cover}
+                      alt="book cover" />
+              </Box>
+              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                  <CardContent sx={{ flex: '1 0 auto' }}>
+                      <Typography component="div" variant="h5">
+                          {props.title}
+                      </Typography>
+                      <Typography variant="subtitle1" color="black" component="div">
+                          {props.author}
+                      </Typography>
+                  </CardContent>
+              </Box>
+          </Card></>
+          <Typography variant="h5" sx={{display:'left', alignItems:'left'}}>What did you think?</Typography>
+          <Box component="span" sx={{ p: 20, border: '1px dashed black' }}>
+          </Box>
+          <Button variant="contained" sx={{backgroundColor:'orange'}}>POST</Button>
+</>
+  );
+}
