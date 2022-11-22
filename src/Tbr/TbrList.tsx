cea56ac42a96191e008e6e122c7484008a5d8186
Eloisamarie29 <eloisamarie.desucatan@cit.edu>
2022-11-22 09:44:27 +0800

    zazaza

--- src/Tbr/TbrList.tsx
+++ src/Tbr/TbrList.tsx
@@ -18,11 +18,13 @@ export default function MediaCard() {
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+          Title
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+          Author | Published
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Synopsis
         </Typography>
       </CardContent>
       <CardActions>
